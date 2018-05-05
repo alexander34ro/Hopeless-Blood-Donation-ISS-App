0cb8dff692dbd4922be516e6bed54815b9dc3a1d
alexander34ro <ardeleanalexandru3@gmail.com>
2018-05-05 08:29:32 +0300

    Update cerere

---
+++ src/main/java/Persistence/DetaliiCerereEntity.java
@@ -0,0 +1,117 @@
+package Persistence;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DetaliiCerere", schema = "main", catalog = "")
+public class DetaliiCerereEntity {
+    private short id;
+    private short cerere;
+    private String componentaSanguina;
+    private String tipSange;
+    private String prioritate;
+    private short completata;
+    private String dataCompletare;
+
+    @Id
+    @Column(name = "id")
+    public short getId() {
+        return id;
+    }
+
+    public void setId(short id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "cerere")
+    public short getCerere() {
+        return cerere;
+    }
+
+    public void setCerere(short cerere) {
+        this.cerere = cerere;
+    }
+
+    @Basic
+    @Column(name = "componentaSanguina")
+    public String getComponentaSanguina() {
+        return componentaSanguina;
+    }
+
+    public void setComponentaSanguina(String componentaSanguina) {
+        this.componentaSanguina = componentaSanguina;
+    }
+
+    @Basic
+    @Column(name = "tipSange")
+    public String getTipSange() {
+        return tipSange;
+    }
+
+    public void setTipSange(String tipSange) {
+        this.tipSange = tipSange;
+    }
+
+    @Basic
+    @Column(name = "prioritate")
+    public String getPrioritate() {
+        return prioritate;
+    }
+
+    public void setPrioritate(String prioritate) {
+        this.prioritate = prioritate;
+    }
+
+    @Basic
+    @Column(name = "completata")
+    public short getCompletata() {
+        return completata;
+    }
+
+    public void setCompletata(short completata) {
+        this.completata = completata;
+    }
+
+    @Basic
+    @Column(name = "dataCompletare")
+    public String getDataCompletare() {
+        return dataCompletare;
+    }
+
+    public void setDataCompletare(String dataCompletare) {
+        this.dataCompletare = dataCompletare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DetaliiCerereEntity that = (DetaliiCerereEntity) o;
+
+        if (id != that.id) return false;
+        if (cerere != that.cerere) return false;
+        if (completata != that.completata) return false;
+        if (componentaSanguina != null ? !componentaSanguina.equals(that.componentaSanguina) : that.componentaSanguina != null)
+            return false;
+        if (tipSange != null ? !tipSange.equals(that.tipSange) : that.tipSange != null) return false;
+        if (prioritate != null ? !prioritate.equals(that.prioritate) : that.prioritate != null) return false;
+        if (dataCompletare != null ? !dataCompletare.equals(that.dataCompletare) : that.dataCompletare != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) id;
+        result = 31 * result + (int) cerere;
+        result = 31 * result + (componentaSanguina != null ? componentaSanguina.hashCode() : 0);
+        result = 31 * result + (tipSange != null ? tipSange.hashCode() : 0);
+        result = 31 * result + (prioritate != null ? prioritate.hashCode() : 0);
+        result = 31 * result + (int) completata;
+        result = 31 * result + (dataCompletare != null ? dataCompletare.hashCode() : 0);
+        return result;
+    }
+}
