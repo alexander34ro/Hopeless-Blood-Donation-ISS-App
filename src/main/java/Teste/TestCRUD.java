890fccf8823999c89ae40f379da944e2a92fb657
BaraianIulia <36893930+BaraianIulia@users.noreply.github.com>
2018-05-21 20:59:43 +0300

    Asistent stuff

---
+++ src/main/java/Teste/TestCRUD.java
@@ -0,0 +1,57 @@
+package Teste;
+
+import Persistence.DonatorEntity;
+import Persistence.PacientEntity;
+import Persistence.SpitalEntity;
+import Services.DumbService;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCRUD {
+
+    @Test
+    public void testSpital() {
+        DumbService dumbService = new DumbService();
+        List<SpitalEntity> spitalEntities = dumbService.getAll(SpitalEntity.class);
+        if (spitalEntities.size() > 0) {
+            SpitalEntity spitalEntity = spitalEntities.get(spitalEntities.size() - 1);
+            dumbService.delete(spitalEntity);
+            List<SpitalEntity> spitalEntities1 = dumbService.getAll(SpitalEntity.class);
+            assertEquals(spitalEntities.size(), spitalEntities1.size() + 1);
+            dumbService.save(spitalEntity);
+            List<SpitalEntity> spitalEntities2 = dumbService.getAll(SpitalEntity.class);
+            assertEquals(spitalEntities.size(), spitalEntities2.size());
+        }
+    }
+    @Test
+    public void testPacient() {
+        DumbService dumbService = new DumbService();
+        List<PacientEntity> pacientEntities = dumbService.getAll(PacientEntity.class);
+        if (pacientEntities.size() > 0) {
+            PacientEntity pacientEntity = pacientEntities.get(pacientEntities.size() - 1);
+            dumbService.delete(pacientEntity);
+            List<PacientEntity> pacientEntities1 = dumbService.getAll(PacientEntity.class);
+            assertEquals(pacientEntities.size(), pacientEntities1.size() + 1);
+            dumbService.save(pacientEntity);
+            List<PacientEntity> pacientEntities2 = dumbService.getAll(PacientEntity.class);
+            assertEquals(pacientEntities.size(), pacientEntities2.size());
+        }
+    }
+    @Test
+    public void testDonator() {
+        DumbService dumbService = new DumbService();
+        List<DonatorEntity> donatorEntities = dumbService.getAll(DonatorEntity.class);
+        if (donatorEntities.size() > 0) {
+            DonatorEntity donatorEntity = donatorEntities.get(donatorEntities.size() - 1);
+            dumbService.delete(donatorEntity);
+            List<DonatorEntity> donatorEntities1= dumbService.getAll(DonatorEntity.class);
+            assertEquals(donatorEntities.size(), donatorEntities1.size() + 1);
+            dumbService.save(donatorEntities);
+            List<DonatorEntity> donatorEntities2 = dumbService.getAll(DonatorEntity.class);
+            assertEquals(donatorEntities.size(), donatorEntities2.size());
+        }
+    }
+}
---
+++ src/main/java/Teste/TestLogin.java
@@ -0,0 +1,32 @@
+package Teste;
+
+import Networking.ClientController;
+import Networking.Interfaces.ServerInterface;
+import Networking.NetworkException;
+import Persistence.MedicEntity;
+import Services.DumbService;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+public class TestLogin {
+    @Test
+    public void  testLogin(){
+        DumbService dumbService=new DumbService();
+        List<MedicEntity> medicEntityList=dumbService.getAll(MedicEntity.class);
+        if(medicEntityList.size()>0){
+            MedicEntity medicEntity=medicEntityList.get(medicEntityList.size()-1);
+            try {
+                ClientController clientController = new ClientController( (ServerInterface) (new ClassPathXmlApplicationContext("classpath:clientSpring.xml")).getBean("bloodDonationService") );
+                MedicEntity medicEntity1= (MedicEntity) clientController.login(medicEntity.getUsername(),medicEntity.getParola());
+                assert(medicEntity1.getNume().equals(medicEntity.getNume()));
+            } catch (RemoteException e) {
+
+            } catch (NetworkException e) {
+
+            }
+        }
+    }
+}
