86e3db5fa30d6e4dc8ac7873baf85a9a1c3ad1c2
alexander34ro <ardeleanalexandru3@gmail.com>
2018-05-31 20:47:12 +0300

    modificari pacienti

--- src/main/java/Persistence/PacientEntity.java
+++ src/main/java/Persistence/PacientEntity.java
@@ -1,16 +1,20 @@
 package Persistence;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "Pacient", schema = "main", catalog = "")
-public class PacientEntity implements java.io.Serializable {
+public class PacientEntity implements Serializable {
     private short id;
     private String nume;
     private String prenume;
     private short prioritate;
-    private short cantitate;
     private MedicEntity medicByMedic;
+    private Short trombociteNecesare;
+    private Short globuleRosiiNecesare;
+    private Short plasmaNecesara;
+    private Short sangeNecesar;
 
     @Id
     @Column(name = "id")
@@ -52,16 +56,6 @@ public class PacientEntity implements java.io.Serializable {
         this.prioritate = prioritate;
     }
 
-    @Basic
-    @Column(name = "cantitate")
-    public short getCantitate() {
-        return cantitate;
-    }
-
-    public void setCantitate(short cantitate) {
-        this.cantitate = cantitate;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -71,7 +65,6 @@ public class PacientEntity implements java.io.Serializable {
 
         if (id != that.id) return false;
         if (prioritate != that.prioritate) return false;
-        if (cantitate != that.cantitate) return false;
         if (nume != null ? !nume.equals(that.nume) : that.nume != null) return false;
         if (prenume != null ? !prenume.equals(that.prenume) : that.prenume != null) return false;
 
@@ -84,7 +77,6 @@ public class PacientEntity implements java.io.Serializable {
         result = 31 * result + (nume != null ? nume.hashCode() : 0);
         result = 31 * result + (prenume != null ? prenume.hashCode() : 0);
         result = 31 * result + (int) prioritate;
-        result = 31 * result + (int) cantitate;
         return result;
     }
 
@@ -96,5 +88,45 @@ public class PacientEntity implements java.io.Serializable {
 
     public void setMedicByMedic(MedicEntity medicByMedic) {
         this.medicByMedic = medicByMedic;
+    }
+
+    @Basic
+    @Column(name = "trombociteNecesare")
+    public Short getTrombociteNecesare() {
+        return trombociteNecesare;
+    }
+
+    public void setTrombociteNecesare(Short trombociteNecesare) {
+        this.trombociteNecesare = trombociteNecesare;
+    }
+
+    @Basic
+    @Column(name = "globuleRosiiNecesare")
+    public Short getGlobuleRosiiNecesare() {
+        return globuleRosiiNecesare;
+    }
+
+    public void setGlobuleRosiiNecesare(Short globuleRosiiNecesare) {
+        this.globuleRosiiNecesare = globuleRosiiNecesare;
+    }
+
+    @Basic
+    @Column(name = "plasmaNecesara")
+    public Short getPlasmaNecesara() {
+        return plasmaNecesara;
+    }
+
+    public void setPlasmaNecesara(Short plasmaNecesara) {
+        this.plasmaNecesara = plasmaNecesara;
+    }
+
+    @Basic
+    @Column(name = "sangeNecesar")
+    public Short getSangeNecesar() {
+        return sangeNecesar;
+    }
+
+    public void setSangeNecesar(Short sangeNecesar) {
+        this.sangeNecesar = sangeNecesar;
     }
 }
