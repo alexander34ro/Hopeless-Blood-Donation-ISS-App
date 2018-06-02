3e2bb573ace10c874095ad56c154dac2163f259b
BaraianIulia <36893930+BaraianIulia@users.noreply.github.com>
2018-06-02 16:25:14 +0300

    u

--- src/main/java/Teste/TestPacient.java
+++ src/main/java/Teste/TestPacient.java
@@ -1,31 +1,46 @@
 package Teste;
 
+import Controllers.CerereController;
+import Controllers.MedicController;
 import Networking.ClientController;
 import Networking.Interfaces.ServerInterface;
 import Networking.NetworkException;
-import Persistence.AsistentEntity;
+import Persistence.MedicEntity;
 import Services.DumbService;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.rmi.RemoteException;
-import java.util.List;
 
 public class TestPacient {
 
     @Test
-    public  void testPacient(){
-        DumbService dumbService=new DumbService();
-        List<AsistentEntity> asistentEntityListt= dumbService.getAll(AsistentEntity.class);
-        if(asistentEntityListt.size()>0){
-            AsistentEntity asistentEntity=asistentEntityListt.get(0);
-            try {
-                ClientController clientController = new ClientController( (ServerInterface) (new ClassPathXmlApplicationContext("classpath:clientSpring.xml")).getBean("bloodDonationService") );
-                clientController.login(asistentEntity.getUsername(),asistentEntity.getParola());
-            } catch (RemoteException e) {
-            } catch (NetworkException e) {
-            }
+    public void testPacient() {
+        DumbService dumbService = new DumbService();
+        MedicEntity medicEntity = dumbService.getAll(MedicEntity.class).get(0);
 
+        ClientController clientController=null;
+        MedicController medicController=null;
+        CerereController cerereController=null;
+        try {
+            clientController = new ClientController((ServerInterface) (new ClassPathXmlApplicationContext("classpath:clientSpring.xml")).getBean("bloodDonationService"));
+
+            MedicEntity medicEntity1 = (MedicEntity) clientController.login(medicEntity.getUsername(), medicEntity.getParola());
+            assert (medicEntity1.getNume().equals(medicEntity.getNume()));
+
+            clientController.setMedicController(medicController);
+            cerereController.setMedic(medicEntity);
+            cerereController.setClient(clientController);
+
+
+
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NetworkException e) {
+            e.printStackTrace();
         }
+
+
     }
 }
