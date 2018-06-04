f29ebe5625dfa667b733cc83baf8ec3f650002eb
BaraianIulia <36893930+BaraianIulia@users.noreply.github.com>
2018-06-04 13:16:19 +0300

    am mai facut chestii pe la AsistentController,mai trebe da nu foarte mult

---
+++ src/main/java/Controllers/AdaugaPacientController.java
@@ -0,0 +1,100 @@
+package Controllers;
+
+import Models.Prioritate;
+import Models.TipSange;
+import Networking.Interfaces.ClientInterface;
+import Networking.NetworkException;
+import Persistence.MedicEntity;
+import Persistence.PacientEntity;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.rmi.RemoteException;
+
+public class AdaugaPacientController {
+    private ClientInterface client;
+    MedicEntity medic = null;
+
+    ObservableList<TipSange> tipSange = FXCollections.observableArrayList();
+    ObservableList<Prioritate> prioritate = FXCollections.observableArrayList();
+
+    @FXML
+    private ComboBox comboBoxPrioritate;
+    @FXML
+    private ComboBox comboBoxTipSange;
+    @FXML
+    private TextField textFieldNume;
+    @FXML
+    private TextField textFieldPrenume;
+    @FXML
+    private TextField textFieldTrombocite;
+    @FXML
+    private TextField textFieldGlobule;
+    @FXML
+    private TextField textFieldPlasma;
+    @FXML
+    private TextField textFieldSange;
+
+    Stage stage;
+
+    MedicController medicCtrl = null;
+
+    public void setMedic(MedicEntity medic) {
+        this.medic = medic;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void setFereastraMedic(MedicController controller) {
+        this.medicCtrl = controller;
+
+    }
+
+    public void setClient(ClientInterface client) {
+        this.client = client;
+        tipSange.setAll(TipSange.values());
+        comboBoxTipSange.setItems(tipSange);
+        prioritate.setAll(Prioritate.values());
+        comboBoxPrioritate.setItems(prioritate);
+        comboBoxTipSange.getSelectionModel().selectFirst();
+        comboBoxPrioritate.getSelectionModel().selectFirst();
+    }
+
+    public void handleAdauga() {
+        PacientEntity pacientEntity = new PacientEntity();
+        if (textFieldGlobule.getText().equals("") || textFieldNume.getText().equals("")|| textFieldPlasma.getText().equals("") || textFieldPrenume.getText().equals("")  || textFieldSange.getText().equals("")  || textFieldTrombocite.getText().equals("") ) {
+            JOptionPane.showMessageDialog(null, "Date necompletate");
+        } else {
+            pacientEntity.setNume(textFieldNume.getText());
+            pacientEntity.setPrenume(textFieldPrenume.getText());
+            pacientEntity.setGlobuleRosiiNecesare(Short.valueOf(textFieldGlobule.getText()));
+            pacientEntity.setMedicByMedic(medic);
+            pacientEntity.setPlasmaNecesara(Short.valueOf(textFieldPlasma.getText()));
+            pacientEntity.setSangeNecesar(Short.valueOf(textFieldSange.getText()));
+            pacientEntity.setTrombociteNecesare(Short.valueOf(textFieldTrombocite.getText()));
+            String prioritate = comboBoxPrioritate.getSelectionModel().getSelectedItem().toString();
+            short prioritatee = 0;
+            if (prioritate == "Medie") prioritatee = 1;
+            else if (prioritate == "Mare") prioritatee = 2;
+            else prioritatee = 3;
+            pacientEntity.setPrioritate(Short.parseShort(String.valueOf(prioritatee)));
+            pacientEntity.setTipSange(comboBoxTipSange.getSelectionModel().getSelectedItem().toString());
+            try {
+                client.saveOrUpdate(pacientEntity);
+            } catch (NetworkException e) {
+                e.printStackTrace();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            medicCtrl.actualizareTabel();
+            stage.close();
+        }
+    }
+}
--- src/main/java/Controllers/MedicController.java
+++ src/main/java/Controllers/MedicController.java
@@ -1,9 +1,15 @@
 package Controllers;
 
+import Models.Prioritate;
+import Models.TipSange;
 import Networking.Interfaces.ClientInterface;
-import Persistence.*;
+import Persistence.CerereEntity;
+import Persistence.DetaliiCerereEntity;
+import Persistence.MedicEntity;
+import Persistence.PacientEntity;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -26,6 +32,8 @@ public class MedicController implements IUserController<MedicEntity> {
         this.user = user;
     }
 
+    ObservableList<TipSange> tipSange = FXCollections.observableArrayList();
+    ObservableList<Prioritate> prioritate = FXCollections.observableArrayList();
     @FXML
     private Label labelNume;
     @FXML
@@ -35,10 +43,9 @@ public class MedicController implements IUserController<MedicEntity> {
     @FXML
     private TableView tableVieww;
 
+
     @FXML
-    private TableColumn<SpitalEntity, String> tableColumnSpital;
-    @FXML
-    private TableColumn<DetaliiCerereEntity, String> tableColumnCantitate;
+    private TableColumn<CerereEntity, String> tableColumnCantitate;
 
     @FXML
     public void initialize() {
@@ -47,6 +54,11 @@ public class MedicController implements IUserController<MedicEntity> {
     public void init() {
         labelNume.setText(user.getNume());
         labelSpital.setText(user.getSpitalBySpital().getNume());
+        actualizareTabel();
+
+    }
+
+    public void actualizareTabel() {
         try {
             tableView.setItems(
                     FXCollections.observableArrayList(
@@ -57,18 +69,14 @@ public class MedicController implements IUserController<MedicEntity> {
                                     .collect(Collectors.toList())
                     )
             );
-            tableColumnSpital.setCellValueFactory(p -> {
-                if (p.getValue() != null) {
-                    return new SimpleStringProperty(p.getValue().getNume());
-                } else
-                    return new SimpleStringProperty("N/A");
-            });
-            List<DetaliiCerereEntity> detaliiCerereEntity=client.getAll(DetaliiCerereEntity.class);
+
+            List<DetaliiCerereEntity> detaliiCerereEntity = client.getAll(DetaliiCerereEntity.class);
             tableColumnCantitate.setCellValueFactory(p -> {
                 if (p.getValue() != null) {
-                    return new SimpleStringProperty(""+detaliiCerereEntity
+                    return new SimpleStringProperty("" + detaliiCerereEntity
                             .stream()
-                            .mapToInt(cerere -> (int) ((DetaliiCerereEntity)cerere).getCantitate())
+                            .filter(object -> object.getCerereByCerere().getId() == p.getValue().getId())
+                            .mapToInt(cerere -> (int) ((DetaliiCerereEntity) cerere).getCantitate())
                             .sum());
 
                 } else
@@ -87,12 +95,36 @@ public class MedicController implements IUserController<MedicEntity> {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
 
     public void setClient(ClientInterface client) {
         this.client = client;
         init();
+    }
+
+    public void handleAdaugaPacient() {
+        Stage stage = new Stage();
+        stage.setTitle("Adauga Pacient");
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("./Views/AdaugaPacient.fxml"));
+
+        Pane pane = null;
+        try {
+            pane = (Pane) loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        AdaugaPacientController ctrl = loader.getController();
+        ctrl.setMedic(user);
+        ctrl.setClient(client);
+        ctrl.setStage(stage);
+        ctrl.setFereastraMedic(this);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+
+
     }
 
     public void handleTrimitere() {
@@ -109,10 +141,12 @@ public class MedicController implements IUserController<MedicEntity> {
 
         CerereController ctrl = loader.getController();
         ctrl.setMedic(user);
+        ctrl.setFereastraMedic(this);
         ctrl.setClient(client);
 
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
+
     }
 }
